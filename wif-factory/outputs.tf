db6159ea3266af18a86b011cf6224d7075513648
AjitPunchhiInutive <167771847+AjitPunchhiInutive@users.noreply.github.com>
2026-02-24 13:56:07 -0500

    Update outputs.tf

--- wif-factory/outputs.tf
+++ wif-factory/outputs.tf
@@ -1,25 +1,46 @@
-#### oytput#####
-
-output "parent_folder_ids" {
-  description = "Map of parent folder keys to folder IDs"
+output "service_accounts" {
+  description = "Map of created service accounts with email and metadata"
   value = {
-    for key, folder in google_folder.parent_folders :
-    key => folder.name
+    for key, sa in google_service_account.github_wif : key => {
+      email       = sa.email
+      unique_id   = sa.unique_id
+      name        = sa.name
+      display_name= sa.display_name
+    }
   }
+  depends_on = [google_service_account.github_wif]
 }
 
-output "sub_folder_ids" {
-  description = "Map of sub-folder keys to folder IDs"
+output "workload_identity_pools" {
+  description = "Map of created Workload Identity Pools"
   value = {
-    for key, folder in google_folder.sub_folders :
-    key => folder.name
+    for key, pool in google_iam_workload_identity_pool.github : key => {
+      name                      = pool.name
+      workload_identity_pool_id = pool.workload_identity_pool_id
+      project_id                = pool.project
+    }
   }
+  depends_on = [google_iam_workload_identity_pool.github]
 }
 
-# output "nested_folders_ids" {
-#   description = "Map of nested-folder keys to folder IDs"
-#   value = {
-#     for key, folder in google_folder.nested_folders :
-#     key => folder.name
-#   }
-# }
+output "workload_identity_providers" {
+  description = "Map of created Workload Identity Providers"
+  value = {
+    for key, provider in google_iam_workload_identity_pool_provider.github : key => {
+      name                  = provider.name
+      provider_id           = provider.workload_identity_pool_provider_id
+      issuer_uri            = provider.oidc[0].issuer_uri
+    }
+  }
+  depends_on = [google_iam_workload_identity_pool_provider.github]
+}
+
+output "sa_wif_mappings" {
+  description = "Mapping of service accounts to WIF provider attributes"
+  value = {
+    for key, mapping in google_service_account_iam_member.github_workload_identity : key => {
+      service_account = mapping.service_account_id
+      member          = mapping.member
+    }
+  }
+}
