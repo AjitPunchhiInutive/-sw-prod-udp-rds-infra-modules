8c190396cc31b6e4d0249b1b82f79247a07c10ba
AjitPunchhiInutive <ajit.punchhi@intuitve.cloud>
2026-03-06 23:25:26 +0530

    adding file

--- serviceaccount/output.tf
+++ serviceaccount/output.tf
@@ -1,43 +1,15 @@
-
-output "service_account_emails" {
-  description = "Email addresses of all created service accounts"
-  value = {
-    for k, sa in google_service_account.sa : k => sa.email
-  }
+output "secret_ids" {
+  description = "Map of secret keys to their full GCP secret resource IDs"
+  value       = { for k, v in google_secret_manager_secret.secrets : k => v.id }
 }
 
-output "service_account_unique_ids" {
-  description = "Unique numeric IDs of all created service accounts"
-  value = {
-    for k, sa in google_service_account.sa : k => sa.unique_id
-  }
-}
-
-output "service_account_key_names" {
-  description = "Full resource names of the generated service account keys"
-  value = {
-    for k, key in google_service_account_key.sa_key : k => key.name
-  }
-}
-
-output "service_account_key_expiry" {
-  description = "Expiry timestamps of the generated keys (empty if no expiry)"
-  value = {
-    for k, key in google_service_account_key.sa_key : k => key.valid_before
-  }
-}
-
-
-output "secret_version_names" {
-  description = "Full resource names of the Secret Manager versions storing each key"
-  value = {
-    for k, sv in google_secret_manager_secret_version.sa_key_version : k => sv.name
-  }
+output "secret_names" {
+  description = "Map of secret keys to their secret_id names"
+  value       = { for k, v in google_secret_manager_secret.secrets : k => v.secret_id }
 }
 
 output "secret_version_ids" {
-  description = "Version IDs within each Secret Manager secret"
-  value = {
-    for k, sv in google_secret_manager_secret_version.sa_key_version : k => sv.version
-  }
+  description = "Map of secret version keys to their full resource IDs"
+  value       = { for k, v in google_secret_manager_secret_version.versions : k => v.id }
+  sensitive   = true
 }
